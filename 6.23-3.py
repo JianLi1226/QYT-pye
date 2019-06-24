408e8f0268880b882dbe2f47c3ee3e5edaf1eed1
JianLi1226 <orchidv524@outlook.com>
2019-06-24 17:36:44 +0200

    try cutting string using index

--- 6.23-3.py
+++ 6.23-3.py
@@ -1,12 +1,16 @@
 print("Pls input any word:")
 word = str(input())
 
-new_list_word = list(word)
-first_cha = new_list_word.pop(0) # pop the first character
+# new_list_word = list(word)
+# first_cha = new_list_word.pop(0) # pop the first character
+#
+# # Note list.append() does not return a new list
+# new_list_word.append('-')  # add a dash at the end of the word
+# new_list_word.append(first_cha)
+# new_list_word.append('y')
+#
+# print('Translated: ' + ''.join(new_list_word))
 
-# Note list.append() does not return a new list
-new_list_word.append('-')  # add a dash at the end of the word
-new_list_word.append(first_cha)
-new_list_word.append('y')
-
-print('Translated: ' + ''.join(new_list_word))
+# much better method
+new_word = word[1:] + word[0] + '-' + 'y'
+print(new_word)
