b3b19b409d44c46cd91647b5bfdc72ca0a586fba
JianLi1226 <orchidv524@outlook.com>
2019-07-01 15:39:42 +0200

    Use findall() to get a list of tuple, with tuple's element is subgroup(substring, braced).

--- Day-6/6.27-1.py
+++ Day-6/6.27-1.py
@@ -8,21 +8,36 @@ asa_conn = "TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, " \
 
 
 sessions_list = list()
+session_dict = dict()
 
 for session in asa_conn.split('\n'):
-    session_parsed = re.match(r'\w*\s\w*\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})\s\w+\s'
+    # session_parsed = re.match(r'\w*\s\w*\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})\s\w+\s'
+    #                           r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})'
+    #                           r'.*bytes (\d+), flags (\w*)', session.strip()).groups()
+    # Try findall()
+    session_parsed = re.findall(r'\w* \w* (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}) \w+ '
                               r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})'
-                              r'.*bytes (\d+), flags (\w*)', session.strip()).groups()
-    session_key = session_parsed[0:4]
-    session_val = session_parsed[4:6]
-    # session_dict = dict() # Have to create a new dict in each loop
-    # session_dict[session_key] = session_val
-    session_dict = {session_key:session_val}
-    sessions_list.append(session_dict)
+                              r'.*bytes (\d+), flags (\w*)', session.strip())
 
-for ses_dict in sessions_list:
-    for key, val in ses_dict.items():
-        print(f'{"src_ip":^10}:{key[0]:^20}|{"src_port":^10}:{key[1]:^10}|'
-              f'{"dst_ip":^10}:{key[2]:^20}|{"dst_port":^10}:{key[3]:^10}')
-        print(f'{"bytes":^10}:{val[0]:^20}|{"flags":^10}:{val[1]:^10}')
-        print("="*110)
+    #print(session_parsed)
+    session_key = session_parsed[0][0:4] # groups() returns a tuple of subgroups, and tuple can be key of a dict.
+    session_val = session_parsed[0][4:6]
+    # session_dict = dict() # Have to create a new dict in each loop
+    session_dict[session_key] = session_val
+    #session_dict = {session_key:session_val}
+    #
+    # sessions_list.append(session_dict)
+
+# for ses_dict in sessions_list:
+#     for key, val in ses_dict.items():
+#         print(f'{"src_ip":^10}:{key[0]:^20}|{"src_port":^10}:{key[1]:^10}|'
+#               f'{"dst_ip":^10}:{key[2]:^20}|{"dst_port":^10}:{key[3]:^10}')
+#         print(f'{"bytes":^10}:{val[0]:^20}|{"flags":^10}:{val[1]:^10}')
+#         print("="*110)
+
+
+for key, val in session_dict.items():
+    print(f'{"src_ip":^10}:{key[0]:^20}|{"src_port":^10}:{key[1]:^10}|'
+            f'{"dst_ip":^10}:{key[2]:^20}|{"dst_port":^10}:{key[3]:^10}')
+    print(f'{"bytes":^10}:{val[0]:^20}|{"flags":^10}:{val[1]:^10}')
+    print("="*110)
